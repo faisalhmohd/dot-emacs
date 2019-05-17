3dec05401454c34a5f349c0036f5e50b6580c17a
Faisal <faisalhmohd@gmail.com>
2019-05-17 15:58:43 +0600

    Split configuration into language specific files

---
+++ config/js.el
@@ -0,0 +1,28 @@
+;; JS Mode
+;; (add-to-list 'auto-mode-alist '("\\.js$" . rjsx-mode))
+
+;; Add TypeScript Support
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  (company-mode +1))
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "jsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+
+(setq-default js-indent-level 2)
