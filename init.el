982648f6340d2dcd247ccf5f3ed8670c3d267b2a
Faisal <faisalhmohd@gmail.com>
2019-06-18 16:55:01 +0600

    Use absolute URL for loading config; Backspace to default behaviour

--- init.el
+++ init.el
@@ -1,14 +1,21 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (setq user-full-name "Mohammad Faisal")
 (setq user-mail-address "faisalhmohd@gmail.com")
 
-(load (expand-file-name "./config/css"))
-(load (expand-file-name "./config/emacs"))
-(load (expand-file-name "./config/html"))
-(load (expand-file-name "./config/js"))
-(load (expand-file-name "./config/lisp"))
-(load (expand-file-name "./config/php"))
-(load (expand-file-name "./config/python"))
-(load (expand-file-name "./config/yaml"))
+(load "~/.emacs.d/config/css")
+(load "~/.emacs.d/config/emacs")
+(load "~/.emacs.d/config/html")
+(load "~/.emacs.d/config/js")
+(load "~/.emacs.d/config/lisp")
+(load "~/.emacs.d/config/php")
+(load "~/.emacs.d/config/python")
+(load "~/.emacs.d/config/yaml")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -23,4 +30,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(region ((t (:background "brightblue" :foreground "#f6f3e8")))))
+
+(global-set-key [(control ?h)] 'delete-backward-char)
