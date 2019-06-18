bfb9aad94ebeaf2b985553de230dad593bddc753
Faisal <faisalhmohd@gmail.com>
2019-06-18 16:55:41 +0600

    Add move-line method

--- config/emacs.el
+++ config/emacs.el
@@ -147,6 +147,19 @@
 ;; Turn off Bell completely
 (setq ring-bell-function 'ignore)
 
+(defun move-line (n)
+  "Move the current line up or down by N lines."
+  (interactive "p")
+  (setq col (current-column))
+  (beginning-of-line) (setq start (point))
+  (end-of-line) (forward-char) (setq end (point))
+  (let ((line-text (delete-and-extract-region start end)))
+    (forward-line n)
+    (insert line-text)
+    ;; restore point to original column in moved line
+    (forward-line -1)
+    (forward-char col)))
+
 ;; Move Line Up and Down
 (defun move-line-up (n)
   "Move the current line up by N lines."
